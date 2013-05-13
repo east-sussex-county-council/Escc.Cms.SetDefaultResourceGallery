284424f5f6d2e5c79e1211adc8cc9871dbe748fc
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-05-13 14:41:00 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using EsccWebTeam.Cms;
+using EsccWebTeam.Cms.Permissions;
+using Microsoft.ApplicationBlocks.ExceptionManagement;
+using Microsoft.ContentManagement.Publishing;
+
+namespace Escc.Cms.SetDefaultResourceGallery
+{
+    /// <summary>
+    /// For the whole CMS site, sets the default resource gallery for each channel to one matching the name of a group with editor permissions for the channel, if it exists.
+    /// </summary>
+    /// <remarks>
+    /// <para>The aim of this tool is to give web authors a hint as to which resource gallery to save resources in. It should be scheduled to run regularly as web author
+    /// permissions, and therefore the appropriate resource gallery, can change. It is important that resources are uploaded to the correct gallery for the following scenario:</para>
+    /// 
+    /// <list type="bullet">
+    /// <item>User A is in Groups 1 and 2, for two sets of pages</item>
+    /// <item>User B is only in Group 2</item>
+    /// <item>If User A saves a resource in the Group 1 gallery which should be in the Group 2 gallery, User B cannot save the page using that resource</item>
+    /// </list>
+    /// </remarks>
+    class Program
+    {
+        private static readonly IList<string> GroupsWithoutAResourceGallery = new List<string>();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var traverser = new CmsTraverser();
+                traverser.TraversingChannel += new CmsEventHandler(traverser_TraversingChannel);
+                traverser.TraverseSite(PublishingMode.Update, true);
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.Publish(ex);
+                throw;
+            }
+        }
+
+        static void traverser_TraversingChannel(object sender, CmsEventArgs e)
+        {
+            Console.WriteLine("Channel: " + e.Channel.UrlModePublished);
+            var groups = CmsPermissions.ReadCmsGroupsForChannel(e.Channel);
+
+            foreach (var groupName in groups[CmsRole.Editor])
+            {
+                // Check for previous failed query, to avoid hundreds of checks on sitewide groups used by Digital Services
+                if (GroupsWithoutAResourceGallery.Contains(groupName))
+                {
+                    continue;
+                }
+
+                var resourceGallery = e.Context.Searches.GetByPath("/Resources/Web authors/" + groupName) as ResourceGallery;
+                if (resourceGallery != null)
+                {
+                    if (e.Channel.DefaultResourceGallery == null ||
+                        e.Channel.DefaultResourceGallery.Guid != resourceGallery.Guid)
+                    {
+                        Console.WriteLine("Setting default resource gallery to " + resourceGallery.Name);
+                        e.Channel.DefaultResourceGallery = resourceGallery;
+                        e.Context.CommitAll();
+                    }
+
+                    return;
+                }
+                else
+                {
+                    GroupsWithoutAResourceGallery.Add(groupName);
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Escc.Cms.SetDefaultResourceGallery")]
+[assembly: AssemblyDescription("Sets the default resource gallery for all CMS channels where web authors are responsible")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("East Sussex County Council")]
+[assembly: AssemblyProduct("Escc.Cms.SetDefaultResourceGallery")]
+[assembly: AssemblyCopyright("Copyright © East Sussex County Council 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e76b44e2-c25c-4964-a5d3-d264d331ed68")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
